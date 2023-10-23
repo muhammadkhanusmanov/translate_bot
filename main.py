92b1c9dd65793fe37984d7937d9a7a2d9be3a9d0
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-10-23 23:26:58 +0500

    translate

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram import Bot,Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboar
 
 from deep_translator import GoogleTranslator
 
+from db import DB
+
 def en_uz(text):
     tr_text = GoogleTranslator(source='en',target='uz').translate(text)
     return tr_text
@@ -20,4 +22,19 @@ def uz_ru(text):
     return tr_text
 
 
-# def en_uz(update:Update, context:CallbackContext):
+def translate(update:Update, context:CallbackContext):
+    bot=context.bot 
+    chat_id=update.message.chat.id
+    text = update.message.text
+    db = DB()
+    til = db.get_lang(chat_id)
+    if til == 'en-uz':
+        tr_text = en_uz(text)
+    elif til == 'uz-en':
+        tr_text = uz_en(text)
+    elif til == 'ru-uz':
+        tr_text = ru_uz(text)
+    else:
+        tr_text = uz_ru(text)
+    bot.send_message(chat_id, tr_text)
+    
