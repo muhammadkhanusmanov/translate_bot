3dedc3b0552204b03068e0b2fb031002c2240b07
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-18 20:27:25 +0500

    command line

--- main.py
+++ main.py
@@ -71,15 +71,37 @@ def enuz(update:Update, context:CallbackContext):
     db.save()
     bot.send_message(chat_id,'Enter text for translation', parse_mode=ParseMode.MARKDOWN)
 
-# def adminpanel(update:Update, context:CallbackContext):
+def adminpanel(update:Update, context:CallbackContext):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    msg = query.message.message_id
+    bot=context.bot
+    command = query.data.split(' ')[0]
+    db = DB()
+    a = db.check_admins(chat_id)
+    if a:
+        btn1 = InlineKeyboardButton('📨Xabar yuborish', callback_data='command sendmsg')
+        btn2 = InlineKeyboardButton('📩Forward yuborish', callback_data='command sendfwd')
+        btn3 = InlineKeyboardButton('👤Admin qo\'shish', callback_data='command addadmin')
+        btn4 = InlineKeyboardButton('📢Kanal qo\'shish', callback_data='command addchannel')
+        btn5 = InlineKeyboardButton('👤Admin o\'chirish', callback_data='command dltadmin')
+        btn6 = InlineKeyboardButton('📢Kanal o\'chirish', callback_data='command dltchannel')
+        btn7 = InlineKeyboardButton('📄Admin va kanallar', callback_data='command list')
+        btn8 = InlineKeyboardButton('📊Statistika', callback_data='command statistik')
+        btn = InlineKeyboardMarkup([[btn1,btn2],[btn3,btn4],[btn5,btn6],[btn7],[btn8]])
+        bot.delete_message(chat_id=chat_id, message_id=msg)
+        bot.sendMessage(chat_id, '*Admin menu*', reply_markup=btn, parse_mode=ParseMode.MARKDOWN)
+    else:
+        bot.sendMessage(chat_id, 'Bad request')
+    db.save()
 
-    
 
 updater=Updater('5873498271:AAGbWIyvaojE9RZ7HafEVDn2zfU8CVEJ_IY')
 
 updater.dispatcher.add_handler(CommandHandler('start',start))
 updater.dispatcher.add_handler(CommandHandler('uzen',uzen))
 updater.dispatcher.add_handler(CommandHandler('enuz',enuz))
+updater.dispatcher.add_handler(CallbackQueryHandler(adminpanel, pattern='admin'))
 updater.dispatcher.add_handler(MessageHandler(Filters.text,translate))
 
 updater.start_polling()
